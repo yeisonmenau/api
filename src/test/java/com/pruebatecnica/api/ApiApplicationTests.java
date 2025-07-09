c51d02fb8ac629aef05734a488ac00908813f7d7
yeisonmenau <yeisonandresdh@gmail.com>
2025-07-08 23:18:10 -0500

    Iniciar proyecto Spring Boot con dependencias JPA, web, H2 y lombok

---
+++ src/test/java/com/pruebatecnica/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
