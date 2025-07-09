67975f0681fcdc0cecddbe4098b98d2d70a26c05
yeisonmenau <yeisonandresdh@gmail.com>
2025-07-08 23:25:19 -0500

    Crear entidad Empleado

---
+++ src/main/java/com/pruebatecnica/api/model/Empleado.java
@@ -0,0 +1,31 @@
+package com.pruebatecnica.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "empleado")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "empleado_id")
+    private Long empleadoId;
+
+    @Column(length = 20)
+    private String cedula;
+
+    private String nombre;
+
+    private Integer edad;
+
+}
