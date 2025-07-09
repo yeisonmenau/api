707d51639f299f62346a2e402e85b08f13ae5a0b
yeisonmenau <yeisonandresdh@gmail.com>
2025-07-08 23:27:28 -0500

    Crear entidad Viaje

---
+++ src/main/java/com/pruebatecnica/api/model/Viaje.java
@@ -0,0 +1,31 @@
+package com.pruebatecnica.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "viaje")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Viaje {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "viaje_id")
+    private Long viajeId;
+
+    @Column(length = 188)
+    private String destino;
+    private LocalDate viajeFecha;
+
+
+}
